abd0cd1924e6f4bffe2a2bb15f547e83c6be9917
Soulaimanelimane <limansoulaiman@gmail.com>
2026-07-23 13:00:10 +0100

    pushing after checking with soufiane

--- srcs/requirement/mariadb/file.sh
+++ srcs/requirement/mariadb/file.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/bin/bash
 
 mysqld_safe --skip-networking &
 
@@ -7,13 +6,13 @@ until mysqladmin ping --silent; do
     sleep 1
 done
 
-mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;"
+mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 
 mysql -e "CREATE USER IF NOT EXISTS 'aglid'@'%' IDENTIFIED BY 'aglid-2005';"
 mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'aglid'@'%';"
 
-mysql -e "CREATE USER IF NOT EXISTS 'isemg'@'%' IDENTIFIED BY 'isemg-1337';"
-mysql -e "GRANT SELECT, INSERT, UPDATE, DELETE ON wordpress.* TO 'isemg'@'%';"
+# mysql -e "CREATE USER IF NOT EXISTS 'isemg'@'%' IDENTIFIED BY 'isemg-1337';"
+# mysql -e "GRANT SELECT, INSERT, UPDATE, DELETE ON wordpress.* TO 'isemg'@'%';"
 
 mysql -e "FLUSH PRIVILEGES;"
 
--- srcs/requirement/wordpress/file.sh
+++ srcs/requirement/wordpress/file.sh
@@ -6,10 +6,10 @@ echo "Waiting for MariaDB to be ready..."
 sleep 10
 echo "MariaDB is fully booted and ready!"
 
-wp config create --dbname=wordpress --dbuser=aglid  --dbpass=aglid-2005 --dbhost=mariadb:3306  --allow-root
+wp config create --dbname=$DB_NAME --dbuser=$USER_NAME  --dbpass=$WP_USER_PASS --dbhost=mariadb:3306  --allow-root
 
-wp core install --url=slimane.42.fr --title=Inception --admin_user=soulaimane --admin_password=Soul1337@ --admin_email=soulaimanliman@gmail.com --allow-root
+wp core install --url=$URL --title=Inception --admin_user=$WP_AD_USER --admin_password=$WP_PS_USER --admin_email=$WP_USER_EMAIL --allow-root
 
-wp user create slimane limansoulaiman@gmail.com --role=subscriber --user_pass=slimane1337/ --allow-root
+wp user create $WP_USER $WP_EMAIL  --role=subscriber --user_pass=$WP_PASS --allow-root
 
 exec "$@"
